de22c4c9ee35500a7104159d0ec769f28c6baae0
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2025-11-06 13:04:28 +0000

    muitos testes mesmo

---
+++ app.js
@@ -0,0 +1,8 @@
+// Passo 1: MOSTRAR a mensagem de boas-vindas
+  console.log("Olá! Bem-vindos à programação! Aprender direito e fazer valer! Vamos ver o máximo que conseguimos aprender para evoluir de forma eficiente!");
+
+  // Passo 2: Mostrar a soma (Processamento Simples)
+  console.log(10 + 4); 
+
+  // Passo 3: Mostrar um valor Booleano (revisão da Aula 1)
+  console.log(true);
