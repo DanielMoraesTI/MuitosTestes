d5ad182970cfe3e6934b0924a6d3bc24dc6ad24c
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2025-11-07 12:55:42 +0000

    Com exercício de média de notas

---
+++ Aula4/exercicio.js
@@ -0,0 +1,17 @@
+const nota1 = 15;
+const nota2 = 20;
+const nota3 = 25;
+
+let soma = nota1 + nota2 + nota3;
+
+let media = soma / 3;
+
+alert('A soma da suas notas é ' + soma);
+console.log('A soma de suas notas é ' + soma);
+
+alert('A sua média das 3 nostas é ' + media);
+console.log('A sua média das 3 nostas é ' + media);
+
+const passou = media > 14;
+alert('Será que você passou na disciplina?' + passou);
+console.log('Será que você passou na disciplina?' + passou);
