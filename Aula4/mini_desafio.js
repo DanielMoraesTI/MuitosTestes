54534a9f5eca0a5802a3c56c83d0959989cddaa1
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2025-11-07 12:43:06 +0000

    Melhoria com let para o anor no mini desafio

--- Aula4/mini_desafio.js
+++ Aula4/mini_desafio.js
@@ -1,7 +1,8 @@
 let nome = prompt('Qual é seu nome?');
 let anoNascimento = prompt('Em qual ano nasceu?');
+let ano = 2025;
 
-let idadeAtual = 2025 - anoNascimento;
+let idadeAtual = ano - anoNascimento;
 
 console.log('Sua idade atualmente é de', idadeAtual, 'anos');
-alert('Sua idade atualmente é de ' + idadeAtual + ' anos ', nome);
+alert('Sua idade atualmente é de ' + idadeAtual + ' anos ' + nome);
